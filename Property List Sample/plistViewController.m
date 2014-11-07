2f3f80b0b623d4cf2f3de822297ffa1a5fb0d2f1
Mubashir Meddekar <mubashirmeddekar@yahoo.com>
2014-11-07 17:34:19 +0530

    Sample Code for pList in iOS 7
    Know about the pList sample here..

--- Property List Sample/plistViewController.m
+++ Property List Sample/plistViewController.m
@@ -18,6 +18,16 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSString *plist = [[NSBundle mainBundle]pathForResource:@"Test" ofType:@"plist"];
+    
+    NSDictionary *strings = [[NSDictionary alloc]initWithContentsOfFile:plist];
+    
+    NSMutableArray *firstarray = [[NSMutableArray alloc]init];
+    
+    [firstarray addObject:[strings valueForKey:@"First"]];
+    
+    NSLog(@"%@",firstarray);
+    
 }
 
 - (void)didReceiveMemoryWarning
